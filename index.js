72b6ae93d40e844450f86a14b8be839ab794cf78
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2017-10-03 04:37:02 -0400

    1) Added counters for GET and POST requests. 2) Added logging to keep track of ongoing requests status and requests counts.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var SERVER_NAME = 'game-api'
 var PORT = 8000;
 var HOST = '127.0.0.1';
 
+var getCounter = 0
+var postCounter = 0
+
 var restify = require('restify')
 
     // Get a persistence engine for the games
@@ -30,8 +33,14 @@ server
 // Get all games in the system
 server.get('/games', function (req, res, next) {
 
+    console.log('\n> /games: received GET request')
+
     // Find every game within the game collection
     gamesSave.find({}, function (error, games) {
+
+        console.log('< /games: sending GET response')
+        getCounter++
+        console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
 
         // Return all of the games in the system
         res.send(games)
@@ -41,8 +50,14 @@ server.get('/games', function (req, res, next) {
 // Get a single game by its game id
 server.get('/games/:id', function (req, res, next) {
 
+    console.log('\n> /games/%s: received GET request', req.params.id)
+
     // Find a single game by its id within save
     gamesSave.findOne({ _id: req.params.id }, function (error, game) {
+
+        console.log('> /games/%s: sending GET response', req.params.id)
+        getCounter++
+        console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
 
         // If there are any errors, pass them to next in the JSON format
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
@@ -59,6 +74,8 @@ server.get('/games/:id', function (req, res, next) {
 
 // Create a new game
 server.post('/games', function (req, res, next) {
+
+    console.log('\n> /games: received POST request')
 
     // Make sure name is defined
     if (req.params.name === undefined) {
@@ -78,6 +95,10 @@ server.post('/games', function (req, res, next) {
     // Create the game using the persistence engine
     gamesSave.create(newGame, function (error, game) {
 
+        console.log('> /games: sending POST response')
+        postCounter++
+        console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
+
         // If there are any errors, pass them to next in the JSON format
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
@@ -88,6 +109,8 @@ server.post('/games', function (req, res, next) {
 
 // Update a game by its id
 server.put('/games/:id', function (req, res, next) {
+
+    console.log('\n> /games/%s: received PUT request', req.params.id)
 
     // Make sure name is defined
     if (req.params.name === undefined) {
@@ -108,6 +131,9 @@ server.put('/games/:id', function (req, res, next) {
     // Update the game with the persistence engine
     gamesSave.update(newGame, function (error, game) {
 
+        console.log('> /games/%s: sending PUT response', req.params.id)
+        console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
+
         // If there are any errors, pass them to next in the JSON format
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
@@ -119,8 +145,13 @@ server.put('/games/:id', function (req, res, next) {
 // Delete game with the given id
 server.del('/games', function (req, res, next) {
 
+    console.log('\n> /games: received DELETE request')
+
     // Delete the game with the persistence engine
     gamesSave.deleteMany({}, function (error, game) {
+
+        console.log('> /games: sending DELETE response')
+        console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
 
         // If there are any errors, pass them to next in the JSON format
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
