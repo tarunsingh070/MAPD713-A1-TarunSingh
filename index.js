868a01f3d9c432140d8886a954b50629739c9b32
tarunsingh070 <31713053+tarunsingh070@users.noreply.github.com>
2017-10-03 21:27:24 -0400

    Removed the term Games from endpoints and other places in the project code and replaced with Products.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var SERVER_NAME = 'game-api'
+var SERVER_NAME = 'product-api'
 var PORT = 8000;
 var HOST = '127.0.0.1';
 
@@ -7,8 +7,8 @@ var postCounter = 0
 
 var restify = require('restify')
 
-    // Get a persistence engine for the games
-    , gamesSave = require('save')('games')
+    // Get a persistence engine for the products
+    , productsSave = require('save')('products')
 
     // Create the restify server
     , server = restify.createServer({ name: SERVER_NAME })
@@ -16,11 +16,11 @@ var restify = require('restify')
 server.listen(PORT, HOST, function () {
     console.log('\nServer %s is listening at %s', server.name, server.url)
     console.log('\nEndpoints:')
-    console.log('http://127.0.0.1:8000/games    method:GET')
-    console.log('http://127.0.0.1:8000/games/:id    method:GET')
-    console.log('http://127.0.0.1:8000/games    method:POST')
-    console.log('http://127.0.0.1:8000/games/:id    method:PUT')
-    console.log('http://127.0.0.1:8000/games    method:DELETE')
+    console.log('http://127.0.0.1:8000/products    method:GET')
+    console.log('http://127.0.0.1:8000/products/:id    method:GET')
+    console.log('http://127.0.0.1:8000/products    method:POST')
+    console.log('http://127.0.0.1:8000/products/:id    method:PUT')
+    console.log('http://127.0.0.1:8000/products    method:DELETE')
 })
 
 server
@@ -30,52 +30,52 @@ server
     // Maps req.body to req.params so there is no switching between them
     .use(restify.bodyParser())
 
-// Get all games in the system
-server.get('/games', function (req, res, next) {
+// Get all products in the system
+server.get('/products', function (req, res, next) {
 
-    console.log('\n> /games: received GET request')
+    console.log('\n> /products: received GET request')
 
-    // Find every game within the game collection
-    gamesSave.find({}, function (error, games) {
+    // Find every product within the product collection
+    productsSave.find({}, function (error, products) {
 
-        console.log('< /games: sending GET response')
+        console.log('< /products: sending GET response')
         getCounter++
         console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
 
-        // Return all of the games in the system
-        res.send(games)
+        // Return all of the products in the system
+        res.send(products)
     })
 })
 
-// Get a single game by its game id
-server.get('/games/:id', function (req, res, next) {
+// Get a single product by its product id
+server.get('/products/:id', function (req, res, next) {
 
-    console.log('\n> /games/%s: received GET request', req.params.id)
+    console.log('\n> /products/%s: received GET request', req.params.id)
 
-    // Find a single game by its id within save
-    gamesSave.findOne({ _id: req.params.id }, function (error, game) {
+    // Find a single product by its id within save
+    productsSave.findOne({ _id: req.params.id }, function (error, product) {
 
-        console.log('> /games/%s: sending GET response', req.params.id)
+        console.log('> /products/%s: sending GET response', req.params.id)
         getCounter++
         console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
 
         // If there are any errors, pass them to next in the JSON format
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-        if (game) {
-            // Send the game if no issues
-            res.send(game)
+        if (product) {
+            // Send the product if no issues
+            res.send(product)
         } else {
-            // Send 404 header if the game doesn't exist
+            // Send 404 header if the product doesn't exist
             res.send(404)
         }
     })
 })
 
-// Create a new game
-server.post('/games', function (req, res, next) {
+// Create a new product
+server.post('/products', function (req, res, next) {
 
-    console.log('\n> /games: received POST request')
+    console.log('\n> /products: received POST request')
 
     // Make sure name is defined
     if (req.params.name === undefined) {
@@ -87,30 +87,30 @@ server.post('/games', function (req, res, next) {
         return next(new restify.InvalidArgumentError('price must be supplied'))
     }
 
-    var newGame = {
+    var newProduct = {
         name: req.params.name,
         price: req.params.price
     }
 
-    // Create the game using the persistence engine
-    gamesSave.create(newGame, function (error, game) {
+    // Create the product using the persistence engine
+    productsSave.create(newProduct, function (error, product) {
 
-        console.log('> /games: sending POST response')
+        console.log('> /products: sending POST response')
         postCounter++
         console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
 
         // If there are any errors, pass them to next in the JSON format
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-        // Send the game if no issues
-        res.send(201, game)
+        // Send the product if no issues
+        res.send(201, product)
     })
 })
 
-// Update a game by its id
-server.put('/games/:id', function (req, res, next) {
+// Update a product by its id
+server.put('/products/:id', function (req, res, next) {
 
-    console.log('\n> /games/%s: received PUT request', req.params.id)
+    console.log('\n> /products/%s: received PUT request', req.params.id)
 
     // Make sure name is defined
     if (req.params.name === undefined) {
@@ -122,41 +122,41 @@ server.put('/games/:id', function (req, res, next) {
         return next(new restify.InvalidArgumentError('price must be supplied'))
     }
 
-    var newGame = {
+    var newProduct = {
         _id: req.params.id,
         name: req.params.name,
         price: req.params.price
     }
 
-    // Update the game with the persistence engine
-    gamesSave.update(newGame, function (error, game) {
+    // Update the product with the persistence engine
+    productsSave.update(newProduct, function (error, product) {
 
-        console.log('> /games/%s: sending PUT response', req.params.id)
+        console.log('> /products/%s: sending PUT response', req.params.id)
         console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
 
         // If there are any errors, pass them to next in the JSON format
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
         // Send a 200 OK response
-        res.send(200, newGame)
+        res.send(200, newProduct)
     })
 })
 
-// Delete game with the given id
-server.del('/games', function (req, res, next) {
+// Delete product with the given id
+server.del('/products', function (req, res, next) {
 
-    console.log('\n> /games: received DELETE request')
+    console.log('\n> /products: received DELETE request')
 
-    // Delete the game with the persistence engine
-    gamesSave.deleteMany({}, function (error, game) {
+    // Delete the product with the persistence engine
+    productsSave.deleteMany({}, function (error, product) {
 
-        console.log('> /games: sending DELETE response')
+        console.log('> /products: sending DELETE response')
         console.log('Processed Requests count --> GET :%s, POST:%s', getCounter, postCounter)
 
         // If there are any errors, pass them to next in the JSON format
         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
         // Send a 200 OK response
-        res.send(200, 'All Games deleted !')
+        res.send(200, 'All Products deleted !')
     })
 })
